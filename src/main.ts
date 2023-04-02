03ec4ef8438379ee947f5e8d11593699e33684b6
Aleksandr Panteleev <a.panteleev@atol.ru>
2023-04-02 23:30:58 +0300

    feat: add logger and some minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3001);
+
+  const configService = app.get(ConfigService);
+  if (configService.get('NODE_ENV') === 'development') {
+    app.enableCors();
+  }
+
+  const port = configService.get('PORT');
+  await app.listen(port);
+  const logger = new Logger('bootstrap');
+  logger.log(`Application listens on the port: ${port}`);
 }
 bootstrap();
