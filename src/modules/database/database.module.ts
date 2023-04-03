48f31a29048b8a0351f6b5b2d01cf6c8abf3986c
Aleksandr Panteleev <a.panteleev@atol.ru>
2023-04-04 01:24:46 +0300

    ci: fix deploy

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -9,13 +9,6 @@ import { Product } from '../products/products.model';
     {
       provide: 'SEQUELIZE',
       useFactory: async (configService: ConfigService) => {
-        console.log({
-          host: configService.get<string>('DB_HOST'),
-          port: configService.get<number>('DB_PORT'),
-          username: configService.get<string>('DB_USERNAME'),
-          password: configService.get<string>('DB_PASSWORD'),
-          database: configService.get<string>('DB_NAME'),
-        });
         const sequelize = new Sequelize({
           dialect: 'postgres',
           host: configService.get<string>('DB_HOST'),
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from './products.model';
 
-@Controller('api/products')
+@Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
